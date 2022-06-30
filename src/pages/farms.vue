653136e79fecc3e1c50aedaf7be93d9a1bae71db
RDMStreet <RDMStreetOne@gmail.com>
2022-06-30 09:25:28 +0300

    SAccordionItem bug

--- src/pages/farms.vue
+++ src/pages/farms.vue
@@ -1,11 +1,21 @@
-<script lang="ts">
-export default {
-  name: 'Farms',
-}
+<script setup lang="ts" name="Farms">
+import { SAccordionItem } from '@soramitsu-ui/ui'
+
+const args = ref({
+  title: 'Accordion item',
+  subtitle: 'Accordion item description',
+})
+
+const model = ref(false)
 </script>
 
 <template>
-  <center>Page in the process of implementation</center>
+  <SAccordionItem v-bind="args" v-model="model">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur.
+  </SAccordionItem>
 </template>
 
 <route lang="yaml">
